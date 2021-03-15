f91f34cd24f224f48f9a46d6298de4ad8f4c9c27
Starei <dima.brishten2001@gmail.com>
2021-03-15 17:53:49 +0300

    фиксация: "lab1/Brishten_Dima

---
+++ Reports/Brishten_Dima/Lab2/1.js
@@ -0,0 +1,23 @@
+while (true) 
+{
+    var num1 = prompt("Введите первое число");
+    var num2 = prompt("Введите второе число");
+
+    if (isNaN(num1))
+    {
+        alert("Первый ввод не число");
+        break;
+    }
+    if (isNaN(num2))
+    {
+        alert("Второй ввод не число");
+        break;
+    }
+
+    if (num1 == num2)
+        alert("Числа равны");
+    else if (num1 > num2)
+        alert("Второе число меньше");
+    else
+        alert("Первое число меньше") 
+}
