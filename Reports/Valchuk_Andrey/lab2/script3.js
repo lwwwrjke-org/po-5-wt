4331160506a7e5fdf05769605fa3b7b6834db447
AndreyValchuk <123214867a@gmail.com>
2021-03-16 05:25:53 +0300

    Added description for input and exception handler

--- Reports/Valchuk_Andrey/lab2/script3.js
+++ Reports/Valchuk_Andrey/lab2/script3.js
@@ -1,21 +1,30 @@
 function getFibNumber() {
-    let index = Number(prompt());
+    let index = Number(prompt("Введите номер числа в последовательности Фибоначчи"));
     
+    try {
+        validateIndex(index);
+        
+        if (index === 1) {
+            alert(0);
+            return;
+        }
+
+        alert(fibonacci(index - 1));
+    }
+    catch (e) {
+        alert(e);
+    }
+}
+
+function fibonacci(n) {
+    return (n <= 1) ? n : fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+function validateIndex(index) {
     if (Number.isNaN(index)) {
         throw "Введено не число";
     }
     else if (index <= 0) {
         throw "Число не может быть меньшим либо равным 0";
     }
-
-    if (index === 1) {
-        alert(0);
-        return;
-    }
-
-    alert(fibonacci(index - 1));
-}
-
-function fibonacci(n) {
-    return (n <= 1) ? n : fibonacci(n - 1) + fibonacci(n - 2);
 }
