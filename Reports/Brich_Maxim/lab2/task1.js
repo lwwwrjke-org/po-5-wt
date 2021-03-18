058dbdd054b6aaed7c30665466d5221f2dc20608
Himik03 <m.brich1@mail.ru>
2021-03-18 22:59:58 +0300

    lab2/Brich_Maxim

---
+++ Reports/Brich_Maxim/lab2/task1.js
@@ -0,0 +1,23 @@
+﻿function task1(){
+    while (true){
+        let x1 = Number(prompt("Введите число #1"));
+        if (Number.isNaN(x1)){
+            alert("Первый ввод - не число");
+            return;
+        }
+
+        let x2 = Number(prompt("Введите число #2"));
+        if (Number.isNaN(x2)){
+            alert("Второй ввод - не число");
+            return;
+        }
+
+        if (x1 == x2){
+            alert("Числа равны");
+        } else if (x1 < x2){
+            alert("Первое число меньше");
+        } else if (x1 > x2){
+            alert("Второе число меньше");
+        };
+    }
+}  
