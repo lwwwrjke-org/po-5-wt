9589890f785ae9c567874d0667c3ecbbade9fb57
Himik03 <78737436+Himik03@users.noreply.github.com>
2021-03-22 13:54:52 +0300

    lab2/Brich_Maxim (#34)

---
+++ Reports/Brich_Maxim/lab2/task3.js
@@ -0,0 +1,15 @@
+﻿function task3(){
+    let num_i = Number(prompt("Введите номер элемента числа Фибоначчи(целые числа от нуля)"));
+	if(Number.isNaN(num_i) || num_i < 0 || !Number.isInteger(num_i)) {
+		error();
+		return;
+	}
+	alert("под номером "+ num_i+" находится число "+Search_num(num_i));
+}
+function Search_num(num_i){
+	if (num_i == 0|| num_i == 1) {return 1}
+	return Search_num(num_i-2) +Search_num(num_i-1);
+}
+function error(){
+	alert("Не верные входные данные!!!");
+}
---
+++ Reports/Brich_Maxim/lab2/task4.js
@@ -0,0 +1,23 @@
+﻿function task4(){
+	let DaysOfWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+	
+    let month = Number(prompt("Введите номер месяца (от 1 до 12)") - 1);
+	if (!valid(month,0,11)) return;
+	
+    let day = Number(prompt("Введите число месяца (от 1 до 31)"));
+	if (!valid(day,1,31)) return;
+    let year = 2016;
+	
+	
+    let date = new Date(year, month, day);
+    if (date.getFullYear() == year && date.getMonth() == (month) && date.getDate() == day) 
+		alert(day+"."+(month+1)+"."+year+" было(а) "+DaysOfWeek[date.getDay()]);
+    else alert("В этом месяце нет такого дня!");
+}
+function valid(num, min,max){
+	if(Number.isNaN(num)||num < min || num > max|| !Number.isInteger(num)) {
+		alert("Не верные входные данные!!!");
+		return false;
+	}
+	return true;
+}
