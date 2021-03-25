f4fb0ded0dbd9d173be5821a761d09ca422cbdef
Radionchik Svetlana <greenbay654@gmail.com>
2021-03-25 22:25:55 +0300

    Modified script1

--- Reports/Radionchik_Svetlana/lab3/script1.js
+++ Reports/Radionchik_Svetlana/lab3/script1.js
@@ -2,16 +2,18 @@ function task1() {
     let StrtOfRng = Number(prompt("Введите начало массива"));
     let EndOfRng = Number(prompt("Введите конец массива"));
     let Step = Number(prompt("Введите шаг для построение массива или будет рассматриваться шаг по умолчанию (нулевой шаг воспринимается как шаг по умолчанию)"));
-    if (Step == 0 && StrtOfRng-EndOfRng<0) {
+    if (Step == 0 && StrtOfRng-EndOfRng < 0) {
         alert("["+range(StrtOfRng, EndOfRng)+"]");
     }
-    else if (Step == 0 && StrtOfRng-EndOfRng>0) {
+    else if (Step == 0 && StrtOfRng-EndOfRng > 0) {
         alert("["+range(StrtOfRng, EndOfRng, -1)+"]");
     }
-    else if (Step < 0 && StrtOfRng-EndOfRng<0) {
+    else if (Step < 0 && StrtOfRng-EndOfRng < 0) {
+        alert("Данные введены неправильно. Строим массив с шагом по умолчанию для данного диапазона.");
         alert("["+range(StrtOfRng, EndOfRng)+"]");
     }
-    else if (Step > 0 && StrtOfRng-EndOfRng>0) {
+    else if (Step > 0 && StrtOfRng-EndOfRng > 0) {
+        alert("Данные введены неправильно. Строим массив с шагом по умолчанию для данного диапазона.");
         alert("["+range(StrtOfRng, EndOfRng, -1)+"]");
     }
     else {
@@ -20,13 +22,13 @@ function task1() {
 }
 
 function range(StrtOfRng, EndOfRng, Step = 1) {
-    let i=StrtOfRng;
-    let RngArr=new Array();
-    if (Step<0) {
+    let i = StrtOfRng;
+    let RngArr = new Array();
+    if (Step < 0) {
         StrtOfRng=EndOfRng;
         EndOfRng=i;
     }
-    for( ;i>=StrtOfRng && i<=EndOfRng; i+=Step) {
+    for( ;i >= StrtOfRng && i <= EndOfRng; i += Step) {
         RngArr.push(i);
     }
     return RngArr;
