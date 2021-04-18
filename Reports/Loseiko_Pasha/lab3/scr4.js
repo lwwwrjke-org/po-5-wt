bfb86fea46ee69973350c10f305e9c2c3c295fb7
pavel420 <zdsdcd@gmail.com>
2021-04-18 22:12:03 +0300

    lab3/Loseiko_Pasha

---
+++ Reports/Loseiko_Pasha/lab3/scr4.js
@@ -0,0 +1,18 @@
+function union(...matrixs){
+    var mass = [], res = {};
+
+    for (var i = 0; i < matrixs.length; i++) {
+        mass = mass.concat(matrixs[i]);
+    }
+
+    for (var i = 0; i < mass.length; i++) {
+        var id = mass[i];
+        res[id] = true;
+    }
+    return Object.keys(res);
+}
+
+function unification(){
+    alert("Матрицы: " + " [78, 25, 52, 1], [112, 24, 25, 1], [1, 25]");
+    alert("Результат: " + "[" + union( [78, 25, 52, 1], [112, 24, 25, 1], [1, 25]) + "]");
+} 
