472d73f7136f15cf2ab068331ab4cff28580c042
Kirill Smekalov <smekalovkirill@gmail.com>
2021-05-17 19:57:41 +0300

    Files added

---
+++ Reports/Smekalov_Kirill/lab2/task4.js
@@ -0,0 +1,33 @@
+function st4() 
+{
+    var day = prompt("Введите день (от 1 до 31)");
+    if (isNaN(day)) {
+        alert("Данные не число");
+        fail;
+    } else {
+        day = Number.parseInt(day);
+        if (day < 1 || day > 31) {
+            alert("Неверное число");
+            fail;
+        }
+    }
+    var month = prompt("Введите месяц (от 1 до 12)");
+    if (isNaN(month)) {
+        alert("Данные не число");
+        fail;
+    } else {
+        month = Number.parseInt(month);
+        month--;
+        if (month < 0 || month > 11) {
+            alert("Неверное число");
+            fail;
+        }
+    }
+    var name_days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+
+    const year = 2016;
+
+    let time = new Date(year, month, day);
+    if (time.getMonth() === (month) && time.getDate() === day && time.getFullYear() === year) alert(`это было(а) ${name_days[time.getDay()]}`);
+    else alert("В этом месяце нет такого дня!");
+} 
