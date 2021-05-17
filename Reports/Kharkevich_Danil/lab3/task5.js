057b0c97340a8bffa804f97634913f6653f2aca2
lordofcreate <danilharkevich59@gmail.com>
2021-05-18 01:10:40 +0300

    lab3/Kharkevich_Danil: lab3 added

---
+++ Reports/Kharkevich_Danil/lab3/task5.js
@@ -0,0 +1,16 @@
+function task5(){
+var arr1 = [1, [2,[3,[4]]]];
+var arr2 = [1, [2], [3, [[4]]],[5,6]];
+function flatten(arr){
+	var i = 0;
+	while(i < arr.length){
+		arr = arr.reduce(function(a,b){
+			return a.concat(b);
+		}, []);
+		i++;
+	}
+	return arr;
+}
+alert(flatten(arr1));
+alert(flatten(arr2));
+}  
