1933411b0d359601687d7cd456ec2c51c455cea6
Kiryl Knysh <57794545+10k20@users.noreply.github.com>
2021-05-21 18:18:08 +0300

    Delete folder

--- Reports/lab5/src/Components/Icon/Icon.js
+++
@@ -1,9 +0,0 @@
-const Icon = (props) => {
-    return (
-        <div className="Icon">
-            <i className={"fas fa-" + props.icon + " fa-2x"}></i>
-        </div>
-    )
-}
-
-export default Icon
